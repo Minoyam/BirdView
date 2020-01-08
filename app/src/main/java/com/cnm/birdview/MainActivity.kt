6d930d12b828611bd858c1cbb659f5a7d1e99526
mino <k3000js@naver.com>
2020-01-08 21:14:06 +0900

    화장품 검색기능 추가 / UX
    처음 시작시 키보드 숨김 / 검색 완료시 키보드 숨김 / 검색 내용 없음 토스트

--- app/src/main/java/com/cnm/birdview/MainActivity.kt
+++ app/src/main/java/com/cnm/birdview/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.cnm.birdview
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.cnm.birdview.network.NetworkHelper
+import com.cnm.birdview.network.ProductsResponse
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,17 +28,35 @@ class MainActivity : AppCompatActivity() {
             adapter = productsAdapter
             layoutManager = GridLayoutManager(this@MainActivity, 2)
         }
-        productsShow()
+        et_search.setOnEditorActionListener { _, i, _ ->
+            when (i) {
+                EditorInfo.IME_ACTION_SEARCH -> {
+                    if (et_search.text.toString().isNotEmpty()) {
+                        productsShow(NetworkHelper.productsApi.getSearchProducts(et_search.text.toString()))
+                    } else {
+                        Toast.makeText(this, "검색 내용이 없습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    et_search.hideKeyboard()
+                }
+            }
+            true
+        }
+        productsShow(NetworkHelper.productsApi.getAllProducts())
 
     }
 
-    private fun productsShow() {
+    private fun productsShow(sc: Observable<ProductsResponse>) {
         disposable.add(
-            NetworkHelper.productsApi.getAllProducts()
+            sc
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
                     productsAdapter.setItem(it.body)
                 }
         )
+    }
+
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
--- app/src/main/java/com/cnm/birdview/network/ProductsApi.kt
+++ app/src/main/java/com/cnm/birdview/network/ProductsApi.kt
@@ -2,8 +2,12 @@ package com.cnm.birdview.network
 
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProductsApi {
     @GET("products")
-    fun getAllProducts():Observable<ProductsResponse>
+    fun getAllProducts(): Observable<ProductsResponse>
+
+    @GET("products?search")
+    fun getSearchProducts(@Query("search") search: String): Observable<ProductsResponse>
 }
