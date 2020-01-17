96ac47cc7f3f7b5419244cb4d402f13e3ddd85ff
mino <k3000js@naver.com>
2020-01-18 01:00:58 +0900

    헤더  / 스피너 구현
    목록 아래로 이동할 때 사라지고, 위로 이동할 때 나타나는 헤더를 구현
    스피너 기능 추가해야됨

--- app/src/main/java/com/cnm/birdview/MainActivity.kt
+++ app/src/main/java/com/cnm/birdview/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
@@ -16,23 +18,58 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(),ProductsAdapter.ItemOnClickListener {
+class MainActivity : AppCompatActivity(), ProductsAdapter.ItemOnClickListener {
 
     private val productsAdapter = ProductsAdapter(this@MainActivity)
     private val disposable = CompositeDisposable()
+
     override fun itemOnClick(productsItem: ProductsResponse.Body) {
         val intent = Intent(this, ProductsDetail::class.java)
         intent.putExtra("product", productsItem.id)
         startActivity(intent)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val spinnerItems = resources.getStringArray(R.array.sort_array)
+        val spinnerAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
 
+        sp_sort.adapter = spinnerAdapter
         rv_content.apply {
             adapter = productsAdapter
             layoutManager = GridLayoutManager(this@MainActivity, 2)
         }
+
+        sp_sort.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+
+                //아이템이 클릭 되면 맨 위부터 position 0번부터 순서대로 동작하게 됩니다.
+                when (position) {
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                    //...
+                    else -> {
+
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+        }
+
         et_search.setOnEditorActionListener { _, i, _ ->
             when (i) {
                 EditorInfo.IME_ACTION_SEARCH -> {
