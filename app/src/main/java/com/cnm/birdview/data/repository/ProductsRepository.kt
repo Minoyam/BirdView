8397624b2e9d753f815dad657c4042747c491deb
mino <k3000js@naver.com>
2020-01-19 20:49:28 +0900

    repository 구축

--- app/src/main/java/com/cnm/birdview/data/repository/ProductsRepository.kt
+++ app/src/main/java/com/cnm/birdview/data/repository/ProductsRepository.kt
@@ -1,4 +1,19 @@
 package com.cnm.birdview.data.repository
 
+import com.cnm.birdview.data.model.ProductsDetailResponse
+import com.cnm.birdview.data.model.ProductsResponse
+import io.reactivex.Observable
+
 interface ProductsRepository {
+    fun getAllProducts(): Observable<ProductsResponse>
+
+    fun getNextPageProducts(
+        skin_type: String, page: Int
+    ): Observable<ProductsResponse>
+
+    fun getSearchProducts(search: String): Observable<ProductsResponse>
+
+    fun getIdProducts(id: Int): Observable<ProductsDetailResponse>
+
+    fun getSortProducts(skin_type: String): Observable<ProductsResponse>
 }
--- app/src/main/java/com/cnm/birdview/data/repository/ProductsRepositoryImp.kt
+++ app/src/main/java/com/cnm/birdview/data/repository/ProductsRepositoryImp.kt
@@ -1,2 +1,30 @@
 package com.cnm.birdview.data.repository
 
+import com.cnm.birdview.data.model.ProductsDetailResponse
+import com.cnm.birdview.data.model.ProductsResponse
+import com.cnm.birdview.data.remote.ProductsRemoteDataSource
+import io.reactivex.Observable
+
+class ProductsRepositoryImp(private val remoteDataSource: ProductsRemoteDataSource) :
+    ProductsRepository {
+    override fun getAllProducts(): Observable<ProductsResponse> {
+        return remoteDataSource.getAllProducts()
+    }
+
+    override fun getNextPageProducts(skin_type: String, page: Int): Observable<ProductsResponse> {
+        return remoteDataSource.getNextPageProducts(skin_type, page)
+    }
+
+    override fun getSearchProducts(search: String): Observable<ProductsResponse> {
+        return remoteDataSource.getSearchProducts(search)
+    }
+
+    override fun getIdProducts(id: Int): Observable<ProductsDetailResponse> {
+        return remoteDataSource.getIdProducts(id)
+    }
+
+    override fun getSortProducts(skin_type: String): Observable<ProductsResponse> {
+        return remoteDataSource.getSortProducts(skin_type)
+    }
+
+}
